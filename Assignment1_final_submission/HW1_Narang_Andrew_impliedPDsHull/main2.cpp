bc7b40fb9f0da321e0c74efc23967cc8205fa41e
an444 <anarang@fordham.edu>
2019-12-02 18:47:14 -0500

    created term structures of hazard rates for each issuer. placed them and implied pds per period along with corresponding yields in separate text files. good for usage in creating weighted average term structures of hazard rates now.

--- Assignment1_final_submission/HW1_Narang_Andrew_impliedPDsHull/main2.cpp
+++ Assignment1_final_submission/HW1_Narang_Andrew_impliedPDsHull/main2.cpp
@@ -7,12 +7,13 @@
 #include <iostream>
 #include <iomanip>
 #include <vector>
+#include <fstream>
 
 #include "IR0.hpp"
 #include "Bond.hpp"
 
 int main()
-{
+{ 
   {
     //testing of new ImpliedPD functionality within the Bond class using Prof's posted spreadsheet
     int freq = 2;
@@ -100,14 +101,39 @@ int main()
     double GE1px = 99.863/100.0;
     double GE1y = GE1.Yield(GE1px, 0.1, 0.000001);
 
-    std::cout << "GE1 yield: " << GE1y << std::endl;
+    std::string target0("GE_implied_pds.txt");
+    std::string target1("GE_hzds.txt");
+    
+    std::ofstream GE_ipds(target0);
+
+    if (GE_ipds.fail())
+      {
+	std::cerr << "could not write " << target0 << '\n';
+	return -1;
+      }
+
+    std::ofstream GE_hzds(target1);
+
+    if (GE_hzds.fail())
+      {
+	std::cerr << "could not write " << target1 << '\n';
+	return -2;
+      }
+    
+    GE_ipds << "GE1 yield: " << GE1y << std::endl;
 
     std::vector<double> zeros1(mats1.size(), rf_1yr); 
     
     double GE1_pd = GE1.ImpliedPD(zeros1, rr0, GE1y);
 
-    std::cout << "GE1 implied pd: " << GE1_pd << std::endl;
+    GE_ipds << "GE1 implied pd: " << GE1_pd << std::endl;
 
+    ///calculation of corresponding hazard rates
+
+    double GE01_hzd = hzd(GE1_pd, 0.0);
+
+    GE_hzds << "GE01 hazard rate: " << GE01_hzd << std::endl;
+    
     //GE 3year
     //matures on 10/09/22
     //next coupon date is on 04/09/2020
@@ -124,17 +150,23 @@ int main()
 
     double GE3px = 100.307/100.0;
     double GE3y = GE3.Yield(GE3px, 0.1, 0.0001);
-    std::cout << "\nGE3 bond yield: " << GE3y << std::endl;
+    GE_ipds << "\nGE3 bond yield: " << GE3y << std::endl;
 
     std::vector<double> zeros3(mats3.size(), rf_3yr);
     
     double GE3_pd = GE3.ImpliedPD(zeros3, rr0, GE3y);
 
-    std::cout << "GE3 implied pd: " << GE3_pd << std::endl;
+    GE_ipds << "GE3 implied pd: " << GE3_pd << std::endl;
 
     double GE13_pd = GE3.ImpliedPD1(zeros3, rr0, GE3y, GE1_pd, 2);
 
-    std::cout << "GE13 intermediate pd: " << GE13_pd << std::endl; 
+    GE_ipds << "GE13 intermediate pd: " << GE13_pd << std::endl; 
+
+    //calculation of corresponding hazard rate for yrs 1-3
+
+    double GE13_hzd = hzd(GE13_pd, GE1_pd);
+
+    GE_hzds << "GE13 hazard rate: " << GE13_hzd << std::endl;
     
     //GE 5yr
     //matures on 11/15/24
@@ -152,23 +184,52 @@ int main()
 
     double GE5px = 100.957/100.0;
     double GE5y = GE5.Yield(GE5px, 0.1, 0.0001);
-    std::cout << "\nGE5 bond yield: " << GE5y << std::endl;
+    GE_ipds << "\nGE5 bond yield: " << GE5y << std::endl;
 
     std::vector<double> zeros5(mats5.size(), rf_5yr);
 
     double GE5_pd = GE5.ImpliedPD(zeros5, rr0, GE5y);
 
-    std::cout << "GE5 implied pd: " << GE5_pd << std::endl;
+    GE_ipds << "GE5 implied pd: " << GE5_pd << std::endl;
 
     //next 3 pmts are within approx. a year, so we use the implied unconditional pd from GE1 for them                                  
     double GE35_pd = GE5.ImpliedPD2(zeros5, rr0, GE5y, GE1_pd, 3, GE13_pd, 7);
 
-    std::cout << "GE35 intermediate pd: " << GE35_pd << std::endl;
+    GE_ipds << "GE35 intermediate pd: " << GE35_pd << std::endl;
+
+    //calculation of hazard rate corresponding to GE yrs 3-5
+    double GE35_hzd = hzd(GE35_pd, GE3_pd);
+
+    GE_hzds << "GE35 hazard rate: " << GE35_hzd << std::endl;
     
+    GE_ipds.close();
+    GE_hzds.close();
   }
 
-  std::cout << "\nA = AAPL:" << std::endl;
   {
+    std::string target0("AAPL_implied_pds.txt");
+    std::string target1("AAPL_hzds.txt");
+
+    std::ofstream AAPL_ipds(target0);
+
+    if (AAPL_ipds.fail())
+      {
+        std::cerr << "could not write " << target0 << '\n';
+        return -1;
+      }
+
+    std::ofstream AAPL_hzds(target1);
+
+    if (AAPL_hzds.fail())
+      {
+        std::cerr << "could not write " << target1 << '\n';
+        return -2;
+      }
+
+    AAPL_ipds << "A = AAPL:" << std::endl;
+
+    AAPL_hzds << "A = AAPL:" << std::endl;
+    
     //AAPL 1 year
     //matures on 11/13/20
     //next coupon date = 11/13/20
@@ -186,13 +247,18 @@ int main()
     double A1px = 100.264/100.0;
     double A1y = A1.Yield(A1px, 0.1, 0.000001);
     
-    std::cout << "A1 yield: " << A1y << std::endl;
+    AAPL_ipds << "A1 yield: " << A1y << std::endl;
     
     std::vector<double> zeros1(mats1.size(), rf_1yr);
     
     double A1_pd = A1.ImpliedPD(zeros1, rr0, A1y);
     
-    std::cout << "A1 implied pd: " << A1_pd << std::endl;
+    AAPL_ipds << "A1 implied pd: " << A1_pd << std::endl;
+
+    //calculation of hazard rate corresponding with AAPL yrs 0-1
+    double A01_hzd = hzd(A1_pd, 0.0);
+
+    AAPL_hzds << "A01 hazard rate: " << A01_hzd << std::endl;
     
     //AAPL 3year
     //matures on 09/11/22
@@ -210,17 +276,22 @@ int main()
     
     double A3px = 100.307/100.0;
     double A3y = A3.Yield(A3px, 0.1, 0.0001);
-    std::cout << "\nA3 bond yield: " << A3y << std::endl;
+    AAPL_ipds << "\nA3 bond yield: " << A3y << std::endl;
     
     std::vector<double> zeros3(mats3.size(), rf_3yr);
     
     double A3_pd = A3.ImpliedPD(zeros3, rr0, A3y);
     
-    std::cout << "A3 implied pd: " << A3_pd << std::endl;
+    AAPL_ipds << "A3 implied pd: " << A3_pd << std::endl;
     
     double A13_pd = A3.ImpliedPD1(zeros3, rr0, A3y, A1_pd, 2);
     
-    std::cout << "A13 intermediate pd: " << A13_pd << std::endl;
+    AAPL_ipds << "A13 intermediate pd: " << A13_pd << std::endl;
+
+    //calculation of hazard rate corresponding with AAPL yrs 1-3
+    double A13_hzd = hzd(A13_pd, A1_pd);
+
+    AAPL_hzds << "A13 hazard rate: " << A13_hzd << std::endl;
     
     //AAPL 5yr
     //matures on 05/06/24
@@ -238,23 +309,51 @@ int main()
     
     double A5px = 100.957/100.0;
     double A5y = A5.Yield(A5px, 0.1, 0.0001);
-    std::cout << "\nA5 bond yield: " << A5y << std::endl;
+    AAPL_ipds << "\nA5 bond yield: " << A5y << std::endl;
     
     std::vector<double> zeros5(mats5.size(), rf_5yr);
     
     double A5_pd = A5.ImpliedPD(zeros5, rr0, A5y);
     
-    std::cout << "A5 implied pd: " << A5_pd << std::endl;
+    AAPL_ipds << "A5 implied pd: " << A5_pd << std::endl;
 
     //next 3 pmts are within approx. a year, so we use the implied unconditional pd from A1 for them
     double A35_pd = A5.ImpliedPD2(zeros5, rr0, A5y, A1_pd, 3, A13_pd, 7);
     
-    std::cout << "A35 intermediate pd: " << A35_pd << std::endl;
+    AAPL_ipds << "A35 intermediate pd: " << A35_pd << std::endl;
+
+    //calculation of hazard rate corresponding with AAPL yrs 3-5
+    double A35_hzd = hzd(A35_pd, A3_pd);
+
+    AAPL_hzds << "A35 hazard rate: " << A35_hzd << std::endl;
     
+    AAPL_ipds.close(); AAPL_hzds.close(); 
   }
     
-  std::cout << "\nA = TSLA:" << std::endl;
   {
+    std::string target0("TSLA_implied_pds.txt");
+    std::string target1("TSLA_hzds.txt");
+
+    std::ofstream ipds(target0);
+
+    if (ipds.fail())
+      {
+        std::cerr << "could not write " << target0 << '\n';
+        return -1;
+      }
+
+    std::ofstream hzds(target1);
+
+    if (hzds.fail())
+      {
+        std::cerr << "could not write " << target1 << '\n';
+        return -2;
+      }
+
+    ipds << "A = TSLA:" << std::endl;
+
+    hzds << "A = TSLA:" << std::endl;
+    
     //TSLA 2 year
     //matures on 03/01/21
     //next coupon date = 03/01/20
@@ -272,14 +371,20 @@ int main()
     double A2px = 100.264/100.0;
     double A2y = A2.Yield(A2px, 0.1, 0.000001);
       
-    std::cout << "A2 yield: " << A2y << std::endl;
+    ipds << "A2 yield: " << A2y << std::endl;
       
     std::vector<double> zeros1(mats1.size(), rf_2yr);
       
     double A2_pd = A2.ImpliedPD(zeros1, rr0, A2y);
       
-    std::cout << "A2 implied pd: " << A2_pd << std::endl;
-      
+    ipds << "A2 implied pd: " << A2_pd << std::endl;
+
+    //calculation of hazard rate corresponding with TSLA yrs 0-2
+
+    double A02_hzd = hzd(A2_pd, 0.0);
+
+    hzds << "A02 hazard rate: " << A02_hzd << std::endl;
+    
     //TSLA 3year
     //matures on 03/15/22
     //next coupon date is on 03/15/2020
@@ -296,18 +401,23 @@ int main()
       
     double A3px = 100.307/100.0;
     double A3y = A3.Yield(A3px, 0.1, 0.0001);
-    std::cout << "\nA3 bond yield: " << A3y << std::endl;
+    ipds << "\nA3 bond yield: " << A3y << std::endl;
       
     std::vector<double> zeros3(mats3.size(), rf_3yr);
       
     double A3_pd = A3.ImpliedPD(zeros3, rr0, A3y);
       
-    std::cout << "A3 implied pd: " << A3_pd << std::endl;
+    ipds << "A3 implied pd: " << A3_pd << std::endl;
       
     double A23_pd = A3.ImpliedPD1(zeros3, rr0, A3y, A2_pd, 2);
       
-    std::cout << "A23 intermediate pd: " << A23_pd << std::endl;
-      
+    ipds << "A23 intermediate pd: " << A23_pd << std::endl;
+
+    //calculation of hazard rate corresponding with TSLA yrs 2-3
+    double A23_hzd = hzd(A23_pd, A2_pd);
+
+    hzds << "A23 hazard rate: " << A23_hzd << std::endl;
+
     //TSLA 5yr
     //matures on 05/15/24
     //next coupon date is 12/15/2019
@@ -324,23 +434,51 @@ int main()
       
     double A5px = 100.957/100.0;
     double A5y = A5.Yield(A5px, 0.1, 0.0001);
-    std::cout << "\nA5 bond yield: " << A5y << std::endl;
+    ipds << "\nA5 bond yield: " << A5y << std::endl;
       
     std::vector<double> zeros5(mats5.size(), rf_5yr);
       
     double A5_pd = A5.ImpliedPD(zeros5, rr0, A5y);
       
-    std::cout << "A5 implied pd: " << A5_pd << std::endl;
+    ipds << "A5 implied pd: " << A5_pd << std::endl;
 
     //next 3 pmts are within approx. a year, so we use the implied unconditional pd from A2 for them and the 2 after them
     double A35_pd = A5.ImpliedPD2(zeros5, rr0, A5y, A2_pd, 5, A23_pd, 7); 
       
-    std::cout << "A35 intermediate pd: " << A35_pd << std::endl;
-      
+    ipds << "A35 intermediate pd: " << A35_pd << std::endl;
+
+    //calculation of hazard rate corresponding with TSLA yrs 3-5
+    double A35_hzd = hzd(A35_pd, A3_pd);
+
+    hzds << "A35 hazard rate: " << A35_hzd << std::endl;
+
+    ipds.close(); hzds.close(); 
   }
     
-  std::cout << "\nA = MSFT:" << std::endl;
   {
+    std::string target0("MSFT_implied_pds.txt");
+    std::string target1("MSFT_hzds.txt");
+
+    std::ofstream ipds(target0);
+
+    if (ipds.fail())
+      {
+        std::cerr << "could not write " << target0 << '\n';
+        return -1;
+      }
+
+    std::ofstream hzds(target1);
+
+    if (hzds.fail())
+      {
+        std::cerr << "could not write " << target1 << '\n';
+        return -2;
+      }
+
+    ipds << "A = MSFT:" << std::endl;
+
+    hzds << "A = MSFT:" << std::endl;
+
     //MSFT 1 year
     //matures on 10/01/20
     //next coupon date = 04/01/20
@@ -357,14 +495,18 @@ int main()
     double A1px = 100.264/100.0;
     double A1y = A1.Yield(A1px, 0.1, 0.000001);
       
-    std::cout << "A1 yield: " << A1y << std::endl;
+    ipds << "A1 yield: " << A1y << std::endl;
       
     std::vector<double> zeros1(mats1.size(), rf_1yr);
       
     double A1_pd = A1.ImpliedPD(zeros1, rr0, A1y);
       
-    std::cout << "A1 implied pd: " << A1_pd << std::endl;
-      
+    ipds << "A1 implied pd: " << A1_pd << std::endl;
+
+    //calculation of corresponding hazard rates for MSFT yrs 0-1
+    double A01_hzd = hzd(A1_pd, 0.0);
+    hzds << "A01 hazard rate: " << A01_hzd << std::endl;
+    
     //MSFT 3year
     //matures on 11/15/22
     //next coupon date is on 11/15/2019
@@ -381,19 +523,24 @@ int main()
       
     double A3px = 100.307/100.0;
     double A3y = A3.Yield(A3px, 0.1, 0.0001);
-    std::cout << "\nA3 bond yield: " << A3y << std::endl;
+    ipds << "\nA3 bond yield: " << A3y << std::endl;
       
     std::vector<double> zeros3(mats3.size(), rf_3yr);
     
     double A3_pd = A3.ImpliedPD(zeros3, rr0, A3y);
     
-    std::cout << "A3 implied pd: " << A3_pd << std::endl;
+    ipds << "A3 implied pd: " << A3_pd << std::endl;
 
     //since the next three payments are within roughly a year, we use A1_pd for all three of them
     double A13_pd = A3.ImpliedPD1(zeros3, rr0, A3y, A1_pd, 3);
       
-    std::cout << "A13 intermediate pd: " << A13_pd << std::endl;
+    ipds << "A13 intermediate pd: " << A13_pd << std::endl;
 
+    //calculation of hazard rate corresponding with MSFT yrs 1-3
+    double A13_hzd = hzd(A13_pd, A1_pd);
+
+    hzds << "A13 hazard rate: " << A13_hzd << std::endl;
+    
     //MSFT 5 yr (really more like 4.5 yrs)
     //matures on 02/06/24
     //next coupon date is 02/06/2020
@@ -410,22 +557,51 @@ int main()
       
     double A5px = 100.957/100.0;
     double A5y = A5.Yield(A5px, 0.1, 0.0001);
-    std::cout << "\nA5 bond yield: " << A5y << std::endl;
+    ipds << "\nA5 bond yield: " << A5y << std::endl;
     
     std::vector<double> zeros5(mats5.size(), rf_5yr);
       
     double A5_pd = A5.ImpliedPD(zeros5, rr0, A5y);
       
-    std::cout << "A5 implied pd: " << A5_pd << std::endl;
+    ipds << "A5 implied pd: " << A5_pd << std::endl;
       
     double A35_pd = A5.ImpliedPD2(zeros5, rr0, A5y, A1_pd, 2, A13_pd, 6);
       
-    std::cout << "A35 intermediate pd: " << A35_pd << std::endl;
-      
+    ipds << "A35 intermediate pd: " << A35_pd << std::endl;
+
+    //calculation of hazard rate corresponding with MSFT yrs 3-5
+    double A35_hzd = hzd(A35_pd, A3_pd);
+
+    hzds << "A35 hazard rate: " << A35_hzd << std::endl;
+
+    ipds.close(); hzds.close(); 
   }
     
-  std::cout << "\nA = GS:" << std::endl;
   {
+    std::string target0("GS_implied_pds.txt");
+    std::string target1("GS_hzds.txt");
+
+    std::ofstream ipds(target0);
+
+    if (ipds.fail())
+      {
+        std::cerr << "could not write " << target0 << '\n';
+        return -1;
+      }
+
+    std::ofstream hzds(target1);
+
+    if (hzds.fail())
+      {
+        std::cerr << "could not write " << target1 << '\n';
+        return -2;
+      }
+
+    ipds << "A = GS:" << std::endl;
+
+    hzds << "A = GS:" << std::endl;
+
+    
     //GS 1 year
     //matures on 09/15/20
     //next coupon date = 03/15/20
@@ -442,14 +618,19 @@ int main()
     double A1px = 100.264/100.0;
     double A1y = A1.Yield(A1px, 0.1, 0.000001);
         
-    std::cout << "A1 yield: " << A1y << std::endl;
+    ipds << "A1 yield: " << A1y << std::endl;
         
     std::vector<double> zeros1(mats1.size(), rf_1yr);
         
     double A1_pd = A1.ImpliedPD(zeros1, rr0, A1y);
         
-    std::cout << "A1 implied pd: " << A1_pd << std::endl;
-        
+    ipds << "A1 implied pd: " << A1_pd << std::endl;
+
+    //calculation of hazard rate corresponding with GS yrs 0-1
+    double A01_hzd = hzd(A1_pd, 0.0);
+
+    hzds << "A01 hazard rate: " << A01_hzd << std::endl;
+    
     //GS 3year
     //matures on 10/31/22
     //next coupon date is on 10/31/2019
@@ -466,19 +647,24 @@ int main()
         
     double A3px = 100.307/100.0;
     double A3y = A3.Yield(A3px, 0.1, 0.0001);
-    std::cout << "\nA3 bond yield: " << A3y << std::endl;
+    ipds << "\nA3 bond yield: " << A3y << std::endl;
         
     std::vector<double> zeros3(mats3.size(), rf_3yr);
       
     double A3_pd = A3.ImpliedPD(zeros3, rr0, A3y);
       
-    std::cout << "A3 implied pd: " << A3_pd << std::endl;
+    ipds << "A3 implied pd: " << A3_pd << std::endl;
 
     //next three coupon dates are effectively within a year, so we use A1_pd for all of them
     double A13_pd = A3.ImpliedPD1(zeros3, rr0, A3y, A1_pd, 3);
         
-    std::cout << "A13 intermediate pd: " << A13_pd << std::endl;
+    ipds << "A13 intermediate pd: " << A13_pd << std::endl;
 
+    //calculation of hazard rate corresponding with GS yrs 1-3
+    double A13_hzd = hzd(A13_pd, A1_pd);
+
+    hzds << "A13 hazard rate: " << A13_hzd << std::endl;
+    
     //GS 5 yr
     //matures on 03/03/24
     //next coupon date is 03/03/2020
@@ -495,18 +681,24 @@ int main()
         
     double A5px = 100.957/100.0;
     double A5y = A5.Yield(A5px, 0.1, 0.0001);
-    std::cout << "\nA5 bond yield: " << A5y << std::endl;
+    ipds << "\nA5 bond yield: " << A5y << std::endl;
       
     std::vector<double> zeros5(mats5.size(), rf_5yr);
         
     double A5_pd = A5.ImpliedPD(zeros5, rr0, A5y);
         
-    std::cout << "A5 implied pd: " << A5_pd << std::endl;
+    ipds << "A5 implied pd: " << A5_pd << std::endl;
         
     double A35_pd = A5.ImpliedPD2(zeros5, rr0, A5y, A1_pd, 2, A13_pd, 6); 
         
-    std::cout << "A35 intermediate pd: " << A35_pd << std::endl;
-        
+    ipds << "A35 intermediate pd: " << A35_pd << std::endl;
+
+    //calculation of hazard rate corresponding with GS yrs 3-5
+    double A35_hzd = hzd(A35_pd, A3_pd);
+
+    hzds << "A35 hazard rate: " << A35_hzd << std::endl;
+
+    ipds.close(); hzds.close(); 
   }
     
     
